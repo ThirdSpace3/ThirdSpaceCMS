145744ea738cd18971487501765c1348c5bf2438
Theia Agency <vinc.charles0@gmail.com>
2023-12-06 14:01:55 +0100

    mise en place projet cms

---
+++ production/js/firebaseconfig.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+  import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBHVhd30FIhlOPKoQSP6vZ2slkwtcLsRKU",
+    authDomain: "third--space.firebaseapp.com",
+    databaseURL: "https://third--space-default-rtdb.firebaseio.com",
+    projectId: "third--space",
+    storageBucket: "third--space.appspot.com",
+    messagingSenderId: "99453910780",
+    appId: "1:99453910780:web:bb6bebd9df84f81e7e6179",
+    measurementId: "G-83NKPT3B9E"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const database = getDatabase(app);
+
+  document.querySelectorAll('.selectable-button').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('selected');
+    });
+});
+
+// Function to get the IDs of selected buttons
+function getSelectedButtons() {
+    return Array.from(document.querySelectorAll('.selectable-button.selected'))
+                .map(button => button.id);
+}
+
+// Event listener for the submit button
+document.getElementById('submit-button').addEventListener('click', () => {
+    const selectedButtons = getSelectedButtons();
+    saveSelectedButtonsToFirebase(selectedButtons);
+});
+
+// Function to save selected button IDs to Firebase
+function saveSelectedButtonsToFirebase(selectedButtons) {
+    const dbRef = ref(database, 'selectedButtons');
+    set(dbRef, selectedButtons)
+        .then(() => console.log('Data saved successfully'))
+        .catch(error => console.error('Error saving data:', error));
+}
---
+++ production/js/script.js
@@ -0,0 +1,19 @@
+// document.querySelectorAll('.selectable-button').forEach(function(button) {
+//     button.addEventListener('click', function() {
+//         this.classList.toggle('selected');
+//         console.log(getSelectedButtons()); // Log the selected buttons
+//     });
+// });
+
+// function getSelectedButtons() {
+//     var selectedButtons = [];
+//     var buttons = document.querySelectorAll('.selectable-button');
+
+//     buttons.forEach(function(button) {
+//         if (button.classList.contains('selected')) {
+//             selectedButtons.push(button.id);
+//         }
+//     });
+
+//     return selectedButtons;
+// }
