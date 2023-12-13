a8336a1e7c2be5a60f7416e91cab0f5bdb9ff702
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-12-13 16:59:37 +0100

    login phantom
    Metamask on doing

--- production/js/firebaseconfig.js
+++ production/js/firebaseconfig.js
@@ -1,6 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-  import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+  import { getFirestore, collection, addDoc, updateDoc, doc} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+  
+  // Required for side-effects
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,49 +19,147 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
     measurementId: "G-83NKPT3B9E"
   };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const database = getDatabase(app);
+// Initialize Firebase
+// Firebase Initialization (Assuming firebaseConfig is already defined)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-  document.addEventListener('DOMContentLoaded', () => {
-    // Event listener for the first set of buttons
-    document.querySelectorAll('.selectable-button').forEach(button => {
-        button.addEventListener('click', () => {
-            button.classList.toggle('selected');
-        });
-    });
+let savedDocId = null; // Variable to store the document ID
+let isSecondSetSaved = false; // Flag to indicate if the second set is saved
 
-    // Event listener for the second set of buttons
-    document.querySelectorAll('.selectable-button-2').forEach(button => {
-        button.addEventListener('click', () => {
-            button.classList.toggle('selected');
-        });
-    });
+// Function to toggle button selection
+const toggleButtonSelection = (button) => {
+    if (!isSecondSetSaved) { // Allow toggle only if second set is not saved
+        button.classList.toggle('selected');
+    }
+};
+
+// Add event listeners to buttons
+const buttons = document.querySelectorAll('.selectable-button, .selectable-button-2');
+buttons.forEach(button => {
+    button.addEventListener('click', () => toggleButtonSelection(button));
 });
 
-// Function to get the IDs of selected buttons
-function getSelectedButtons() {
-    return Array.from(document.querySelectorAll('.selectable-button.selected'))
-                .map(button => button.id);
-}
+// Function to get the state of each button
+const getButtonStates = (selector) => {
+    const states = {};
+    const selectedButtons = document.querySelectorAll(selector);
+    selectedButtons.forEach(button => {
+        states[button.id] = button.classList.contains('selected');
+    });
+    return states;
+};
 
-// Function to save selected button IDs to Firebase
-function saveSelectedButtonsToFirebase(selectedButtons) {
-    const dbRef = ref(database, 'selectedButtons');
-    set(dbRef, selectedButtons)
-        .then(() => console.log('Data saved successfully'))
-        .catch(error => console.error('Error saving data:', error));
+// Combined function to handle submit button click
+const handleSubmitButtonClick = async () => {
+    if (!savedDocId) {
+        // Save the initial button states
+        await saveInitialButtonStates();
+        displayEtape2(); // Move to the next step
+    } else if (!isSecondSetSaved) {
+        // Update the document with the second set of button states
+        await updateButtonStatesInFirestore();
+    } else {
+        console.log("Both sets of buttons have already been saved.");
+    }
+};
+
+// Function to save initial button states to Firestore
+const saveInitialButtonStates = async () => {
+    const buttonStates = getButtonStates('.selectable-button');
+
+    try {
+        const docRef = await addDoc(collection(db, "Etapes Buttons"), {
+            firstSetButtons: buttonStates
+        });
+        console.log("Document written with ID: ", docRef.id);
+        savedDocId = docRef.id; // Save the document ID
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+};
+
+// Function to update the document with the second set of button states
+const updateButtonStatesInFirestore = async () => {
+    if (!savedDocId) {
+        console.error("No document ID found. First set of buttons must be saved first.");
+        return;
+    }
+
+    const buttonStates = getButtonStates('.selectable-button-2');
+
+    try {
+        await updateDoc(doc(db, "Etapes Buttons", savedDocId), {
+            secondSetButtons: buttonStates
+        });
+        console.log("Document updated with second set of buttons");
+        isSecondSetSaved = true; // Set the flag to true
+        disableButtons(); // Disable all buttons
+    } catch (e) {
+        console.error("Error updating document: ", e);
+    }
+};
+
+// Function to disable all buttons
+const disableButtons = () => {
+    buttons.forEach(button => {
+        button.disabled = true; // Disable the button
+    });
+};
+
+// Function to display etape2
+function displayEtape2() {
+    const etape1 = document.getElementById("etape1");
+    const etape2 = document.getElementById("etape2");
+
+    etape1.style.display = "none";
+    etape2.style.display = "block";
 }
 
 // Event listener for the submit button
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementById('submit-button').addEventListener('click', () => {
-      const selectedButtons = getSelectedButtons();
-      saveSelectedButtonsToFirebase(selectedButtons);
-      const step1 = document.getElementById("etape1");
-      const step2 = document.getElementById("etape2");
-      step1.style.display = "none";
-      step2.style.display = "block";
-  });
-});
+document.getElementById("submit-button").addEventListener("click", handleSubmitButtonClick);
+
+//#region Provider Phantom
+function getProvider() {
+    if ("solana" in window) {
+        const provider = window.solana;
+        if (provider.isPhantom) {
+            return provider;
+        } else {
+            console.error("Phantom wallet found, but it is not the right provider.");
+        }
+    } else {
+        console.error("Phantom wallet not found. Please install it.");
+    }
+    return null;
+}
+
+// Sign in function
+async function signIn() {
+    const provider = getProvider();
+    if (!provider) {
+        console.error("No provider found.");
+        return;
+    }
+
+    try {
+        const message = `Your custom sign-in message`;
+        const encodedMessage = new TextEncoder().encode(message);
+
+        // Check if the 'signMessage' method is available
+        if (typeof provider.signMessage !== "function") {
+            console.error("signMessage method is not available on the provider.");
+            return;
+        }
+
+        const signedMessage = await provider.signMessage(encodedMessage, "utf8");
+        console.log("Signed Message:", signedMessage);
+        // Handle the signed message here (e.g., send it to your server for verification)
+    } catch (error) {
+        console.error("Error in signing message:", error);
+        // Handle errors (e.g., user refused to sign the message)
+    }
+}
+
+// Add event listener to the Sign In button
+document.getElementById("phantom").addEventListener("click", signIn);
