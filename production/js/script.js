74f84b16246e58f6a378ac807e35f09053a01017
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2023-12-15 21:39:13 +0100

    push navbar

--- production/js/script.js
+++ production/js/script.js
@@ -1,3 +1,26 @@
+//#region TOGGLE MENU
+function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    const menuIcon = document.getElementById('menuIcon');
+    const burgerIconSrc = 'img/navbar-burger.png';
+    const closeIconSrc = 'img/navbar-close.png';
+    const body = document.body;
+  
+    if (menu.classList.contains('menu-open')) {
+      menu.classList.remove('menu-open');
+      body.classList.remove('no-scroll');
+      menuIcon.src = burgerIconSrc;
+    } else {
+      menu.classList.add('menu-open');
+      body.classList.add('no-scroll');
+      menuIcon.src = closeIconSrc;
+    }
+  }
+  //#endregion
+
+
+
+
 // document.querySelectorAll('.selectable-button').forEach(function(button) {
 //     button.addEventListener('click', function() {
 //         this.classList.toggle('selected');
@@ -17,3 +40,4 @@
 
 //     return selectedButtons;
 // }
+
