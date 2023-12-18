52abe2d38135bd91cb75c2a836393d9fd50fb260
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-12-18 16:47:16 +0100

    phatom, metamask, opera
    Infra ledger aussi mais marche pas for now

--- production/js/script.js
+++ production/js/script.js
@@ -17,8 +17,7 @@ function toggleMenu() {
     }
   }
   //#endregion
-
-//POPUP WALLET
+//#region POPUP WALLET
 
   function openPopup() {
     document.getElementById("popup").style.display = "block";
@@ -51,10 +50,130 @@ document.querySelector('.popup .popup-more-btn').addEventListener('click', funct
 });
 
 
+//#endregion
+
+//#region CONNECT WALLET 
+
+// Provider Phantom
+function getProvider() {
+  if ("solana" in window) {
+      const provider = window.solana;
+      if (provider.isPhantom) {
+          return provider;
+      } else {
+          console.error("Phantom wallet found, but it is not the right provider.");
+      }
+  } else {
+      console.error("Phantom wallet not found. Please install it.");
+  }
+  return null;
+}
+
+// Sign in function
+async function signInwithphantom() {
+  const provider = getProvider();
+  if (!provider) {
+      console.error("No provider found.");
+      return;
+  }
+
+  try {
+      const message = `Your custom sign-in message`;
+      const encodedMessage = new TextEncoder().encode(message);
+
+      // Check if the 'signMessage' method is available
+      if (typeof provider.signMessage !== "function") {
+          console.error("signMessage method is not available on the provider.");
+          return;
+      }
+
+      const signedMessage = await provider.signMessage(encodedMessage, "utf8");
+      console.log("Signed Message:", signedMessage);
+      // Handle the signed message here (e.g., send it to your server for verification)
+  } catch (error) {
+      console.error("Error in signing message:", error);
+      // Handle errors (e.g., user refused to sign the message)
+  }
+}
 
 
 
+async function signInWithMetaMask() {
+  if (typeof window.ethereum === 'undefined') {
+      console.error("MetaMask is not installed");
+      return;
+  }
 
+  try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0];
+      const message = `Sign in with Ethereum to the app. Address: ${account}`;
+      const signature = await ethereum.request({ 
+          method: 'personal_sign', 
+          params: [message, account] 
+      });
+
+      console.log("Signed Message:", signature);
+      // Handle the signature here (e.g., send it to your server for verification)
+  } catch (error) {
+      console.error("Error in MetaMask sign-in:", error);
+  }
+}
+
+async function signInWithOpera() {
+  if (window.ethereum && window.ethereum.isOpera) {
+      try {
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+          const account = accounts[0];
+          const message = `Sign in with Opera to the app. Address: ${account}`;
+          const signature = await window.ethereum.request({ 
+              method: 'personal_sign', 
+              params: [message, account] 
+          });
+
+          console.log("Signed Message:", signature);
+          // Handle the signature here
+      } catch (error) {
+          console.error("Error in Opera sign-in:", error);
+      }
+  } else {
+      console.error("Opera Wallet not detected");
+  }
+}
+
+// Ledger integration is more complex due to its hardware nature
+// This is a basic outline; refer to Ledger's documentation for complete integration
+async function signInWithLedger() {
+  const transport = await Transport.create();
+  const eth = new Eth(transport);
+  const path = "44'/60'/0'/0"; // Standard Ethereum HD path
+  const account = await eth.getAddress(path);
+
+  try {
+      const message = `Sign in with Ledger to the app. Address: ${account.address}`;
+      // Convert message to hex format for Ledger
+      const hexMessage = Buffer.from(message).toString("hex");
+      const signature = await eth.signPersonalMessage(path, hexMessage);
+
+      console.log("Signed Message:", signature);
+      // Handle the signature here
+  } catch (error) {
+      console.error("Error in Ledger sign-in:", error);
+  }
+}
+
+
+// Add event listener to the metamask button
+document.getElementById("metamask").addEventListener("click",signInWithMetaMask);
+// Add event listener to the phantom button
+document.getElementById("phantom").addEventListener("click", signInwithphantom);
+// Add event listener to the coinbase button
+document.getElementById("ledger").addEventListener("click", signInWithLedger);
+// Add event listener to the walletcoin button
+document.getElementById("operatouch").addEventListener("click", signInWithOpera);
+
+
+//#endregion
 
 
 // document.querySelectorAll('.selectable-button').forEach(function(button) {
