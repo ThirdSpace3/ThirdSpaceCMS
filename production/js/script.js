aaff6be669f16762ad6c34db4e18d5df921a0d34
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2023-12-20 21:59:12 +0100

    tabs

--- production/js/script.js
+++ production/js/script.js
@@ -175,6 +175,33 @@ async function signInWithOpera() {
   }
 }
 
+
+
+//#region TAB HTML
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Fonction pour ouvrir l'onglet par défaut
+  function openDefaultTab() {
+    document.getElementById("defaultOpen").click();
+  }
+  
+  // Ajouter l'écouteur d'événement pour charger l'onglet par défaut au chargement de la page
+  window.onload = openDefaultTab;
+  
+ //#endregion 
+
 // // Ledger integration is more complex due to its hardware nature
 // // This is a basic outline; refer to Ledger's documentation for complete integration
 // async function signInWithLedger() {
