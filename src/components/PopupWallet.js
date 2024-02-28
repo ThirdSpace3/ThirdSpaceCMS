a560e458b54a56f3d644a199d6b5c0370b8fc1b5
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-02-28 14:58:13 +0100

    popup wallet

--- src/components/PopupWallet.js
+++ src/components/PopupWallet.js
@@ -2,23 +2,25 @@ import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import './PopupWallet.css';
 
-function Popup() {
-    const [isPopupVisible, setIsPopupVisible] = useState(false);
-  
-    // Cette fonction change l'état, ce qui entraîne le rendu conditionnel de la popup
-    const togglePopup = () => {
-      setIsPopupVisible(!isPopupVisible);
-    };
-  
-    return (
-      <>
-        <button onClick={togglePopup}>Toggle Popup</button>
-        {isPopupVisible && (
-          <div className="popup" style={{display: 'block'}}>
-            {/* Contenu de la popup */}
-          </div>
-        )}
-      </>
-    );
-  }
-export default Popup;
+function PopupWallet() {
+  return (
+    <div className="popup" id="popup">
+      <div className="popup-content">
+        <img src="img/navbar-close.png" alt="" className="close-button" onClick={closePopup} />
+        <h2>Connect a wallet</h2>
+        <div className="wallet-list">
+          <button id="metamask" className="wallet-btn"><img src="img/metamask-logo.png" alt="" />Continue with Metamask</button>
+          <button id="phantom" className="wallet-btn"><img src="img/phantom-logo.png" alt="" />Continue with Phantom</button>
+          <button id="ledger" className="wallet-btn"><img src="img/ledger-logo.png" alt="" />Continue with Ledger</button>
+          <button id="operatouch" className="wallet-btn"><img src="img/operatouch-logo.png" alt="" />Continue with Opera Touch</button>
+          <button id="coinbase" className="wallet-btn wallet-more wallet-hide"><img src="img/coinbase-logo.png" alt="" />Continue with Coinbase</button>
+          {/* Hidden for now
+          <button id="Walletconnect" className="wallet-btn wallet-more wallet-hide"><img src="img/walletconnect-logo.png" alt="" />Continue with Walletconnect</button>
+          */}
+        </div>
+        <a href="#" className="popup-more-btn" id="toggleButton" onClick={toggleShowMore}>Show More</a>
+      </div>
+    </div>
+  );
+}
+export default PopupWallet;
