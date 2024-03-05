f2ae3a229de2634aadddb799ac7eebb58c51ba80
Haxxxxxx <vinc.charles0@gmail.com>
2024-03-05 15:24:16 +0100

    update get started buttons

--- src/components/website/home/Header.js
+++ src/components/website/home/Header.js
@@ -1,8 +1,18 @@
 import './Header.css'
 import '../../Root.css'
-import React from 'react';
+import React, { useState } from 'react';
+import PopupWallet from '../PopupWallet';
 
 function Header() {
+  const [showWalletPopup, setShowWalletPopup] = useState(false); // Add this line
+
+  const toggleWalletPopup = () => {
+    setShowWalletPopup(!showWalletPopup);
+  };
+  const handleUserLogin = (userAccount) => {
+    // Handle user login here
+    console.log('User logged in:', userAccount);
+  };
   return (
     <>
     <img src="./images/header-bg.png" alt="" class="header-bg"/>
@@ -12,7 +22,7 @@ function Header() {
           <h1 className="header__title">Web3's Easiest Builder. Dive in Code-Free.</h1>
           <p className="section__text">We, at Third Space, will co-construct the project with you! We want to give everyone the opportunity to boost their business on Web 3.</p>
           <div className="header__cta">
-            <a href="#" className="purple-btn" id="getstarted-btn">Get Started</a>
+            <a className="purple-btn" id="getstarted-btn" onClick={toggleWalletPopup}>Get Started</a>
             <a href="https://discord.gg/dked3DEngT" className="purple-light-btn" target='_blank'>
               <img src="./images/logo-discord-1.png" alt="" className="cta-icon" />Join the community
             </a>
@@ -21,7 +31,10 @@ function Header() {
         <img src="./images/header-img.png" alt="" className="header__right" />
       </section>
       <img src="./images/header-deco.png" alt="" className="header-deco" />
+      {showWalletPopup && <PopupWallet onClose={toggleWalletPopup} onUserLogin={handleUserLogin} />}
+
     </>
+    
   );
 }
 
