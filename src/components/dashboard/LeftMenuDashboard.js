83de7308a882e064bed8721ca06826af7656a23f
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-03-26 15:24:54 +0100

    debut left bar dashboard

---
+++ src/components/dashboard/LeftMenuDashboard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./DashboardMain.css";
+import "../Root.css";
+
+
+export default function LeftMenuDashboard() {
+return (
+     <>
+     <div className="left-menu-container">
+
+        <div className="profile-container">
+            <img src="./images/avatar-placeholder.png"/>
+            <p className="profile-name">Andrew Smith</p>
+
+        </div>
+
+     </div>
+        
+    </>
+    
+);
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,21 +1,18 @@
 import React from "react";
-import "../components/dashboard/Dashboard.css"
-import LeftBar from "../components/dashboard/LeftBar";
-import NavBarDashboard from "../components/dashboard/NavBarDashboard";
-import RecentlyViewed from "../components/dashboard/recentlyViewed";
-import Projectlists from "../components/dashboard/ProjectLists";
+import "../components/dashboard/DashboardMain.css";
+import "../components/Root.css";
+import LeftMenuDashboard from "../components/dashboard/LeftMenuDashboard";
+
+
 
 export default function Dashboard() {
 return (
      <>
-        <div className="Dahsboard_Wrapper">
-                <LeftBar />
-            <div className="Dashboard_Column">
-                <NavBarDashboard />
-                <RecentlyViewed />
-                <Projectlists />
-            </div>
-        </div>
+     <div className="dashboard-container">
+        <LeftMenuDashboard />
+     </div>
+        
+        
     </>
     
 );
