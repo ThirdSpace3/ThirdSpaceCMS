ce2ed90a0672434e2044df4d10f731edb97f3b6f
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-03-28 15:08:49 +0100

    pages manquantes dashboard

---
+++ src/components/dashboard/SiteSettingsDashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SiteSettingsDashboard.css";
+import "../Root.css";
+
+
+
+export default function SiteSettingsDashboard() {
+return (
+     <>
+     <h1>Site Settings</h1>
+        
+        
+    </>
+    
+);
+}
