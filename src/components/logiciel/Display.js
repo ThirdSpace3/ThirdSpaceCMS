4bcfd05073a12ff8cd52eb14f73870e19d5e0131
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-04-15 23:20:22 +0200

    fin front topbar

--- src/components/logiciel/Display.js
+++ src/components/logiciel/Display.js
@@ -1,30 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import LeftBar from './LeftBar';
-import TopBar from './TopBar';
-import RightBar from './RightBar';
-import ActualPageParametersBTN from './ActualPageParametersBTN';
-import './Display.css';
-import { StyleProvider } from '../../hooks/StyleContext';
-import TemplateFullText from '../../templates/TemplateFullText';
-import TemplateImg_txt from '../../templates/TemplateImg_txt';
-import { TemplateTest1 } from '../../templates';
-import axios from 'axios';
-import { ImageHistoryProvider } from '../../hooks/ImageHistoryContext';
-import { useParams } from 'react-router-dom'; // Import useParams
+import React, { useState, useEffect } from "react";
+import LeftBar from "./LeftBar";
+import TopBar from "./TopBar";
+import RightBar from "./RightBar";
+import ActualPageParametersBTN from "./ActualPageParametersBTN";
+import "./Display.css";
+import { StyleProvider } from "../../hooks/StyleContext";
+import TemplateFullText from "../../templates/TemplateFullText";
+import TemplateImg_txt from "../../templates/TemplateImg_txt";
+import { TemplateTest1 } from "../../templates";
+import axios from "axios";
+import { ImageHistoryProvider } from "../../hooks/ImageHistoryContext";
+import { useParams } from "react-router-dom"; // Import useParams
 
 export default function Display() {
-  const [settings, setSettings] = useState({  });
+  const [settings, setSettings] = useState({});
   const [settingsHistory, setSettingsHistory] = useState([settings]);
   const [currentHistoryIndex, setCurrentHistoryIndex] = useState(0);
   const [selectedElement, setSelectedElement] = useState(null);
   const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
-  const [activeEditor, setActiveEditor] = useState(''); // Initialize activeEditor as an empty string
+  const [activeEditor, setActiveEditor] = useState(""); // Initialize activeEditor as an empty string
   const { templateName } = useParams(); // Get templateName from the URL
   useEffect(() => {
     console.log(templateName); // Log the templateName value
     setActiveEditor(templateName); // Set activeEditor to templateName when the component mounts
   }, [templateName]);
-  
 
   const handleSettingsChange = (elementId, newSettings) => {
     // Example function body - adjust based on actual state structure
@@ -36,7 +35,7 @@ export default function Display() {
       return updatedSettings;
     });
   };
-  
+
   const undo = () => {
     if (currentHistoryIndex > 0) {
       setCurrentHistoryIndex(currentHistoryIndex - 1);
@@ -51,45 +50,40 @@ export default function Display() {
     }
   };
 
-
-
-
-
   // This function is to be called when the "Propulse" button is clicked
   const saveSettings = async () => {
-    const walletId = sessionStorage.getItem('userAccount');
+    const walletId = sessionStorage.getItem("userAccount");
     if (!walletId) {
-      alert('No wallet ID found. Please log in.');
+      alert("No wallet ID found. Please log in.");
       return;
     }
 
     try {
       console.log("Sending settings to backend:", settings);
 
-      await axios.post('http://localhost:5000/api/settings', {
-        userId: walletId,
-        settings: settings,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
+      await axios.post(
+        "http://localhost:5000/api/settings",
+        {
+          userId: walletId,
+          settings: settings,
         },
-      });
-      alert('Settings saved successfully');
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      alert("Settings saved successfully");
       setHasUnsavedChanges(false);
     } catch (error) {
-      console.error('Error saving settings:', error);
-      alert('Failed to save settings. See console for more details.');
+      console.error("Error saving settings:", error);
+      alert("Failed to save settings. See console for more details.");
     }
   };
-
-
-
-
 
   const handleEditorChange = (editor) => {
     setActiveEditor(editor);
   };
-
 
   const [selectedDeviceSize, setSelectedDeviceSize] = useState("100%");
 
@@ -111,13 +105,12 @@ export default function Display() {
   const templateComponents = {
     TemplateFullText: TemplateFullText,
     TemplateImg_txt: TemplateImg_txt,
-    TemplateTest1 : TemplateTest1,
+    TemplateTest1: TemplateTest1,
     // Add more templates here as needed
   };
   const FallbackTemplate = () => <div>Template not found.</div>;
   const SelectedTemplate = templateComponents[templateName] || FallbackTemplate;
   console.log(SelectedTemplate); // This should log a function or class if correctly imported
-
 
   return (
     <ImageHistoryProvider>
@@ -132,33 +125,38 @@ export default function Display() {
         <div className="displayWrapper">
           {!isPreviewMode && ( // Hide LeftBar and RightBar in preview mode
             <>
-              <LeftBar handleEditorChange={handleEditorChange} addImageToHistory={addImageToHistory} />
+              <LeftBar
+                handleEditorChange={handleEditorChange}
+                addImageToHistory={addImageToHistory}
+              />
             </>
           )}
           <div className="displayColumnWrapper">
-            {!isPreviewMode && ( // Hide LeftBar and RightBar in preview mode
+            {/* {!isPreviewMode && ( // Hide LeftBar and RightBar in preview mode
               <>
                 <ActualPageParametersBTN />
               </>
-            )}           
-           <SelectedTemplate
-                deviceSize={selectedDeviceSize}
-                settings={settings}
-                handleSettingsChange={handleSettingsChange}
-                selectedElement={selectedElement}
-                setSelectedElement={setSelectedElement}
-                isPreviewMode={isPreviewMode}
-              />
-
+            )}            */}
+            <SelectedTemplate
+              deviceSize={selectedDeviceSize}
+              settings={settings}
+              handleSettingsChange={handleSettingsChange}
+              selectedElement={selectedElement}
+              setSelectedElement={setSelectedElement}
+              isPreviewMode={isPreviewMode}
+            />
           </div>
           {!isPreviewMode && ( // Hide LeftBar and RightBar in preview mode
             <>
-              <RightBar onSettingsChange={handleSettingsChange} selectedElement={selectedElement} addImageToHistory={addImageToHistory}/>
+              <RightBar
+                onSettingsChange={handleSettingsChange}
+                selectedElement={selectedElement}
+                addImageToHistory={addImageToHistory}
+              />
             </>
           )}
         </div>
       </StyleProvider>
     </ImageHistoryProvider>
   );
-
 }
