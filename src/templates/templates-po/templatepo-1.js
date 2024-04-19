06d7eda552ad41d5f5da94a51de5561c08881bc6
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-04-19 22:34:46 +0200

    template responsive

--- src/templates/templates-po/templatepo-1.js
+++ src/templates/templates-po/templatepo-1.js
@@ -46,11 +46,6 @@ const TemplatePo1 = () => {
                   Features
                 </a>
               </li>
-              <li>
-                <a href="" className="sss-product-navbar-links">
-                  Testimonials
-                </a>
-              </li>
             </ul>
             <a href="" className="sss-product-navbar-cta">
               Join Us
@@ -81,11 +76,7 @@ const TemplatePo1 = () => {
                 Features
               </a>
             </li>
-            <li>
-              <a href="" className="sss-product-navbar-links">
-                Testimonials
-              </a>
-            </li>
+
             <li>
               <a href="" className="sss-product-navbar-mobile-cta">
                 Join Us
@@ -239,17 +230,139 @@ const TemplatePo1 = () => {
 
         {/* ===FEATURES=== */}
         <div className="sss-product-features">
-          <h2 className="sss-product-features"></h2>
+          <h2 className="sss-product-features-title">
+            Essential apps that protect your documents
+          </h2>
+          <div className="sss-product-features-box">
+            <div className="sss-product-features-box-top">
+              <div className="sss-product-features-box-top-left">
+                <h3 className="sss-product-features-box-top-left-title">
+                  End-to-end encrypted inbox and messages
+                </h3>
+                <p className="sss-product-features-box-top-left-text">
+                  Rorem ipsum dolor sit amet consectetur. Proin dignissim tortor
+                  mauris viverra sed volutpat mauris. Amet nisi amet commodo
+                  adipiscing ut imperdiet nunc.
+                </p>
+                <a href="" className="sss-product-features-box-top-left-cta">
+                  Join Us
+                </a>
+              </div>
+              <div className="sss-product-features-box-top-right">
+                <img
+                  src="./images/templates-img/3sproduct/3sproduct-feature-1.png"
+                  className="sss-product-features-box-top-right-img"
+                />
+              </div>
+            </div>
+            <div className="sss-product-features-box-bottom">
+              <div className="sss-product-features-box-bottom-left">
+                <img
+                  src="./images/templates-img/3sproduct/3sproduct-feature-2.png"
+                  className="sss-product-features-box-bottom-left-img"
+                />
+                <h3 className="sss-product-features-box-bottom-left-title">
+                  Mobile applications
+                </h3>
+                <p className="sss-product-features-box-bottom-left-text">
+                  Prem ipsum dolor sit amet consectetur. Viverra sed dignissim
+                  risus aliquet condimentum. Vulputate varius feugiat egestas
+                  congue{" "}
+                </p>
+              </div>
+              <div className="sss-product-features-box-bottom-right">
+                <h3 className="sss-product-features-box-bottom-right-title">
+                  Upload, share, and preview any file
+                </h3>
+                <p className="sss-product-features-box-bottom-right-text">
+                  Tellus et adipiscing sit sit mauris pharetra bibendum. Ligula
+                  massa netus nulla ultricies purus.
+                </p>
+                <img
+                  src="./images/templates-img/3sproduct/3sproduct-feature-3.png"
+                  className="sss-product-features-box-bottom-right-img"
+                />
+              </div>
+            </div>
+          </div>
         </div>
 
-        {/* ===TESTIMONIALS=== */}
-        <div className="sss-product-testimonials"></div>
-
         {/* ===JOIN US=== */}
-        <div className="sss-product-joinus"></div>
+        <div className="sss-product-joinus">
+          <img
+            src="./images/templates-img/3sproduct/3sproduct-joinus-1.png"
+            className="sss-product-joinus-logo"
+          />
+          <h2 className="sss-product-joinus-title">Join the community</h2>
+          <p className="sss-product-joinus-text">
+            Join our 400,000+ person community and contribute to a more private
+            and decentralized internet. Start for free.
+          </p>
+          <a href="" className="sss-product-joinus-cta">
+            Join Discord
+          </a>
+        </div>
 
         {/* ===FOOTER=== */}
-        <div className="sss-product-footer"></div>
+        <div className="sss-product-footer">
+          <div className="sss-product-footer-top">
+            <img
+              src="./images/templates-img/3sproduct/3sproduct-logo.png"
+              className="sss-product-footer-top-logo"
+            />
+            <ul className="sss-product-footer-top-links-box">
+              <li>
+                <a href="" className="sss-product-footer-top-links">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="" className="sss-product-footer-top-links">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="" className="sss-product-footer-top-links">
+                  Features
+                </a>
+              </li>
+            </ul>
+            <div className="sss-product-footer-top-rs">
+              <a
+                href="https://twitter.com/BuildWith3S"
+                target="_blank"
+                className="sss-product-footer-top-rs-item"
+              >
+                <img
+                  src="./images/templates-img/3sproduct/3sproduct-footer-1.png"
+                  className="sss-product-footer-top-rs-img"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/company/thirdspace-3/"
+                target="_blank"
+                className="sss-product-footer-top-rs-item"
+              >
+                <img
+                  src="./images/templates-img/3sproduct/3sproduct-footer-4.png"
+                  className="sss-product-footer-top-rs-img"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="sss-product-footer-bottom">
+            <hr className="sss-product-footer-bottom-hr" />
+            <p className="sss-product-footer-bottom-text">
+              Copyright © 3S.Product | Designed inspired by{" "}
+              <span>
+                <a href="https://webocean.io/" target="_blank">
+                  Webocean LTD
+                </a>
+              </span>{" "}
+              - Powered by Third Space
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
