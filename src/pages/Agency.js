c7c06b101d52614e1deeee19b2b21363afc31bb4
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2024-06-02 16:49:44 +0200

    Debut Agency

--- src/pages/Agency.js
+++ src/pages/Agency.js
@@ -43,108 +43,151 @@ export default function Agency() {
 
   return (
     <>
-      <NavBar
-        checkWalletData={checkWalletData}
-        hasWalletData={hasWalletData}
-        accounts={accounts}
-        setAccounts={setAccounts}
-      />
+
       <div className="agency-content">
+
         <div className="agency-header">
-          <h1 className="agency-header-title">3S Agency</h1>
-          <p className="agency-header-text">
-            We, at Third Space, will co-construct the project with you! We want
-            to give everyone the opportunity to boost their business on Web 3.
-          </p>
-          <Link to="#" onClick={scrollToCTA} className="agency-header-cta">Contact Us</Link>
+          <NavBar
+            checkWalletData={checkWalletData}
+            hasWalletData={hasWalletData}
+            accounts={accounts}
+            setAccounts={setAccounts}
+          />
+          <div className="wrapper"> 
+          <div className="agency-header-content">
+            <p>Welcome to 3S Agency</p>
+            <h1 className="agency-header-title">We Create Unique Websites</h1>
+            <p className="agency-header-text">
+              Looking to upscale your digital presence? Look no further. At 3S Agency, we specialize in crafting unique websites tailored to your vision.
+            </p>
+            <Link to="#" onClick={scrollToCTA} className="agency-header-cta">Be Contacted</Link>
+          </div>
+          <div className="agency-header-images">
+            <img src="../..\images\group220.png"></img>
+            <img src=""></img>
+          </div>
+          </div>
         </div>
         <div className="agency-services">
-        <h2 className="agency-services-title">Discover our services</h2>
-          <p className="agency-services-text">
-            We, at Third Space, will co-construct the project with you! We want
-            to give everyone the opportunity to boost their business on Web 3.
-          </p>
+          <h2 className="agency-services-title">Comprehensive Digital Services for Your Business</h2>
           <div className="agency-services-box">
-          <div className="agency-services-item">
-          <i class="bi bi-2-square-fill"></i>
-          <h3 className="agency-services-item-title">Branding</h3>
-          <p className="agency-services-item-text">
-          We build strong, memorable brands that resonate with your target market and differentiate you from the competition.
-          </p>
-
+            <div className="agency-services-item">
+              <i className="bi bi-palette-fill"></i>
+              <h3 className="agency-services-item-title">Creative Design Solutions</h3>
+              <p className="agency-services-item-text">
+                Delivering innovative and visually compelling designs that capture your brand's essence and captivate your audience.
+              </p>
+            </div>
+            <div className="agency-services-item">
+              <i className="bi bi-palette-fill"></i>
+              <h3 className="agency-services-item-title">Unique Branding</h3>
+              <p className="agency-services-item-text">
+                Building a strong and recognizable brand identity that resonates with your target market and drives loyalty.
+              </p>
+            </div>
+            <div className="agency-services-item">
+              <i className="bi bi-palette-fill"></i>
+              <h3 className="agency-services-item-title">Robust Development</h3>
+              <p className="agency-services-item-text">
+                Crafting high-quality and scalable websites and applications tailored to your business needs and user expectations.
+              </p>
+            </div>
+            <div className="agency-services-item">
+              <i className="bi bi-palette-fill"></i>
+              <h3 className="agency-services-item-title">Continuous Updates</h3>
+              <p className="agency-services-item-text">
+                Keeping your digital presence current and secure with regular updates, ensuring optimal performance and relevance.
+              </p>
+            </div>
+            <div className="agency-services-item">
+              <i className="bi bi-palette-fill"></i>
+              <h3 className="agency-services-item-title">Modern Redesign</h3>
+              <p className="agency-services-item-text">
+                Transforming existing websites with a fresh, contemporary look and improved functionality to enhance user experience.
+              </p>
+            </div>
+            <div className="agency-services-item">
+              <i className="bi bi-palette-fill"></i>
+              <h3 className="agency-services-item-title">Seamless Transition</h3>
+              <p className="agency-services-item-text">
+                Ensuring a smooth migration and integration process when moving to new platforms or updating technologies, minimizing downtime and disruption.
+              </p>
+            </div>
           </div>
-          <div className="agency-services-item">
-          <i class="bi bi-2-square-fill"></i>
-          <h3 className="agency-services-item-title">Design</h3>
-          <p className="agency-services-item-text">
-          Our creative team crafts visually stunning and user-friendly designs that captivate your audience and enhance user experience.
-          </p>
-
-          </div>
-          <div className="agency-services-item">
-          <i class="bi bi-2-square-fill"></i>
-          <h3 className="agency-services-item-title">Development</h3>
-          <p className="agency-services-item-text">
-          Whether you need custom-coded solutions or prefer the agility of no-code platforms, our experts deliver robust, scalable, and efficient development services tailored to your needs.
-          </p>
-
-          </div>
-          <div className="agency-services-item">
-          <i class="bi bi-2-square-fill"></i>
-          <h3 className="agency-services-item-title">Update</h3>
-          <p className="agency-services-item-text">
-          Stay current with our comprehensive updating services, ensuring your digital assets are always fresh, secure, and performing at their best.
-          </p>
-
-          </div>
-          <div className="agency-services-item">
-          <i class="bi bi-2-square-fill"></i>
-          <h3 className="agency-services-item-title">Redesign</h3>
-          <p className="agency-services-item-text">
-          Give your existing digital presence a new lease of life with our redesign services, focused on modern aesthetics and improved functionality.
-          </p>
-
-          </div>
-          <div className="agency-services-item">
-          <i class="bi bi-2-square-fill"></i>
-          <h3 className="agency-services-item-title">Transitsion</h3>
-          <p className="agency-services-item-text">
-          Seamlessly migrate from your old systems to new, more efficient platforms with our expert transition services, minimizing downtime and disruption.
-          </p>
-
-          </div>
-
-          </div>
-
         </div>
         <div className="agency-portfolio">
-        <h2 className="agency-portfolio-title">Explore our projects</h2>
-          <p className="agency-portfolio-text">
-            We, at Third Space, will co-construct the project with you! We want
-            to give everyone the opportunity to boost their business on Web 3.
-          </p>
+          <h2 className="agency-portfolio-title">Explore our latest projects</h2>
           <div className="agency-portfolio-box">
-          <div className="agency-portfolio-card">
-            <h3 className="agency-portfolio-card-title">Project Title</h3>
-            <a className="agency-portfolio-card-link">View more <i class="bi bi-arrow-right"></i></a>
-            <p className="agency-portfolio-card-label">Website</p>
-
+            <div className="agency-portfolio-card">
+              <h3 className="agency-portfolio-card-title">Nom du projet</h3>
+              <p className="agency-portfolio-card-text">Lorem ipsum dolor sit amet, Ut opertamise sit rartione offici sit aliquam omnis aut optio datore sit ratione quis</p>
+              <a className="agency-portfolio-card-link" href="#">Go to website <i className="bi bi-arrow-right"></i></a>
+            </div>
+            <div className="agency-portfolio-card">
+              <h3 className="agency-portfolio-card-title">Portfolio Victor Bisch</h3>
+              <p className="agency-portfolio-card-text">Lorem ipsum dolor sit amet, Ut opertamise sit rartione offici sit aliquam omnis aut optio datore sit ratione quis</p>
+              <a className="agency-portfolio-card-link" href="#">Go to website <i className="bi bi-arrow-right"></i></a>
+            </div>
+            <div className="agency-portfolio-card">
+              <h3 className="agency-portfolio-card-title">Nom du projet</h3>
+              <p className="agency-portfolio-card-text">Lorem ipsum dolor sit amet, Ut opertamise sit rartione offici sit aliquam omnis aut optio datore sit ratione quis</p>
+              <a className="agency-portfolio-card-link" href="#">Go to website <i className="bi bi-arrow-right"></i></a>
+            </div>
           </div>
-
-          </div>
-
         </div>
-        <div className="agency-cta" ref={ctaRef}>
-        <h2 className="agency-contact-title">Contact Us</h2>
+        <div className="agency-quality">
+          <h2 className="agency-quality-title">Why Choose Us?</h2>
+          <div className="agency-quality-box">
+            <div className="agency-quality-item">
+              <h3 className="agency-quality-item-title">We design stunning, functional websites tailored to your brand.</h3>
+            </div>
+            <div className="agency-quality-item">
+              <h3 className="agency-quality-item-title">Our responsive development ensures perfect performance on all devices.</h3>
+            </div>
+            <div className="agency-quality-item">
+              <h3 className="agency-quality-item-title">We deliver fast, reliable hosting solutions for optimal site performance.</h3>
+            </div>
+            <div className="agency-quality-item">
+              <h3 className="agency-quality-item-title">We ensure your website has a user-friendly interface for easy navigation.</h3>
+            </div>
+            <div className="agency-quality-item">
+              <h3 className="agency-quality-item-title">We provide ongoing support to keep your website updated and secure.</h3>
+            </div>
+          </div>
+        </div>
+        <div className="agency-expert">
+          <h2 className="agency-expert-title">Meet Your Digital Solutions Expert</h2>
+          <div className="agency-expert-content">
+            <div className="agency-expert-item">
+              <h3 className="agency-expert-item-title">Empowering Businesses with Tailored Solutions</h3>
+              <p className="agency-expert-item-text">
+                Our youthful spirit drives us to empower businesses of all sizes, recognizing that a website is essential, especially for small companies. With our commitment to reasonable pricing and collaborative creation process, we work hand in hand with you to bring your vision to life, ensuring that your digital presence stands out in today's competitive market.
+              </p>
+            </div>
+            <div className="agency-expert-stats">
+              <div className="agency-expert-stat">
+                <h4>15+</h4>
+                <p>Projects</p>
+              </div>
+              <div className="agency-expert-stat">
+                <h4>100%</h4>
+                <p>Satisfaction</p>
+              </div>
+              <div className="agency-expert-stat">
+                <h4>3 Years</h4>
+                <p>Experience</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="agency-contact" ref={ctaRef}>
+          <h2 className="agency-contact-title">Join Our Newsletter</h2>
           <p className="agency-contact-text">
-            We, at Third Space, will co-construct the project with you! We want
-            to give everyone the opportunity to boost their business on Web 3.
+            Join our newsletter for exclusive updates and insights! Rest assured, you can unsubscribe at any time, and we will never sell or share your email with third parties. Read our Privacy Policy for more information.
           </p>
-
-<ContactForm />
+          <ContactForm />
         </div>
       </div>
-
       <Footer />
     </>
   );
