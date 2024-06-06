2810167765136ddfd3d34162296f9125a5425d07
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-06-06 23:08:21 +0200

    modifications 3s agency

--- src/components/website/CarouselPortfolio.js
+++ src/components/website/CarouselPortfolio.js
@@ -33,57 +33,41 @@ const cards = [
   },
 ];
 
+const repeatedCards = Array(15).fill(cards).flat();
+
 const CarouselPortfolio = () => {
   const [index, setIndex] = useState(0);
-  const [isTransitioning, setIsTransitioning] = useState(false);
-  const [touchStartX, setTouchStartX] = useState(null);
-  const [touchEndX, setTouchEndX] = useState(null);
   const ref = useRef();
-  const [isHovering, setIsHovering] = useState(false);
+
+  const nextSlide = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % repeatedCards.length);
+  };
+
+  const prevSlide = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + repeatedCards.length) % repeatedCards.length);
+  };
 
   const handleTouchStart = (e) => {
-    setTouchStartX(e.touches[0].clientX);
+    ref.current.touchStartX = e.touches[0].clientX;
   };
 
   const handleTouchMove = (e) => {
-    setTouchEndX(e.touches[0].clientX);
+    ref.current.touchEndX = e.touches[0].clientX;
   };
 
   const handleTouchEnd = () => {
+    const touchStartX = ref.current.touchStartX;
+    const touchEndX = ref.current.touchEndX;
     if (touchStartX - touchEndX > 100) {
       nextSlide();
     } else if (touchEndX - touchStartX > 100) {
       prevSlide();
     }
-    setTouchStartX(null);
-    setTouchEndX(null);
-  };
-
-  const nextSlide = () => {
-    setIsTransitioning(true);
-    setIndex((prevIndex) => (prevIndex + 1) % cards.length);
-  };
-
-  const prevSlide = () => {
-    setIsTransitioning(true);
-    setIndex((prevIndex) => (prevIndex - 1 + cards.length) % cards.length);
   };
 
   useEffect(() => {
-    setIsTransitioning(false);
-  }, [index]);
-
-  useEffect(() => {
-    const handleResize = () => {
-      ref.current.style.width = `${cards.length * 410}px`;
-    };
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    ref.current.style.width = `${repeatedCards.length * 410}px`;
   }, []);
-  
 
   return (
     <div
@@ -102,19 +86,18 @@ const CarouselPortfolio = () => {
       </div>
 
       <div className="carousel">
-      <div
-  className="carousel-inner"
-  style={{
-    transform: `translateX(-${index * 410}px)`,
-    transition: isTransitioning ? `transform 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)` : 'none',
-  }}
-  ref={ref}
-          
+        <div
+          className="carousel-inner"
+          style={{
+            transform: `translateX(-${index * 410}px)`,
+            transition: 'transform 0.5s ease-in-out',
+          }}
+          ref={ref}
         >
-{[...Array(15).fill(cards).flat()].map((card, i) => (
-              <div key={i} className="carousel-item">
+          {repeatedCards.map((card, i) => (
+            <div key={i} className="carousel-item">
               <div className='carousel-item-img-box'>
-                <img className='carousel-item-img' src={card.image} alt="Third Space 3S Agency Web Agency Affordable Latest Trends" />
+                <img className='carousel-item-img' src={card.image} alt={card.title} />
               </div>
               <div className='carousel-item-box'>
                 <h3 className='carousel-item-title'>{card.title}</h3>
