65a6fad95c7690e30f3090deb6a8f695fd3d61fa
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-06-30 19:26:38 +0200

    Coming Soon footer

--- src/components/website/ComingSoon.js
+++ src/components/website/ComingSoon.js
@@ -1,6 +1,8 @@
 import "./ComingSoon.css";
 import "../Root.css";
 import React from "react";
+import Footer from "../components/website/Footer";
+
 
 export default function ComingSoon() {
   return (
@@ -14,8 +16,8 @@ export default function ComingSoon() {
         <a href="/home" className="purple-btn">
           Go Back Home
         </a>
-        <img src="https://firebasestorage.googleapis.com/v0/b/third--space.appspot.com/o/ImageWebSite%2Fheader-deco.png?alt=media&token=45950dca-a838-4d9f-875e-c3b08d228e44" className="comingsoon-bg" />
-      </div>
+        <Footer />
+        </div>
     </>
   );
 }
